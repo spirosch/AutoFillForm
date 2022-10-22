e5beb517daf1fb0a8eaa3558d0c498fa93a52966
jsomarakis <jason_somarakis@protonmail.com>
2022-10-22 14:54:21 +0300

    commented out actual main

--- actual_main.py
+++ actual_main.py
@@ -4,17 +4,18 @@ from multiprocessing import Process, current_process
 import os
 PROCESS_CAP = 10
 
+# -- 
 
-if __name__ == "__main__":
-    # Set flow
-    #
-    number_of_files = os.listdir('.').__len__()
-    for  iteration in range(1,number_of_files):
-        pass
-
-    # both threads completely executed
-    print("Done!")
-
-    # Parse file (TODO can we do a universal file?)
-    # Create python objects
-    # Upload
+# if __name__ == "__main__":
+#     # Parse file
+#     #
+#     number_of_files = os.listdir('.').__len__()
+#     for  iteration in range(1,number_of_files):
+#         pass
+#
+#     # both threads completely executed
+#     print("Done!")
+#
+#     # Parse file (TODO can we do a universal file?)
+#     # Create python objects
+#     # Upload
--- readme.md
+++ readme.md
@@ -1,6 +1,14 @@
 v0.1 (25/10)
 - Support form 1
 
+Dependencies
+- Folder containing all client data called 'data_form_1'
+  - Each client folder contains a subfolder called 'attachments'
+  - Each client folder contains an .xlsx file called 'data.xlsx' (name can be irrelavant)
+  - The attachment
+Flow
+- For each user 
+  - Parse .xlsx
 
 v0.2 (25/11)
 TODO
